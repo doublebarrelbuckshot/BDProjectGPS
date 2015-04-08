88fa4b76e5b7ccf149965a3a4a2ccab4851f3d2a
Florin&Lavi <Florin&Lavi@MontRoyal2>
2015-04-08 16:08:12 -0400

    autre commit pour verifier le pull apres

--- BD_GPS/tutorial/src/main/java/org/geotools/tutorial/GUI.java
+++ BD_GPS/tutorial/src/main/java/org/geotools/tutorial/GUI.java
@@ -24,7 +24,7 @@ import javax.swing.event.ListSelectionListener;
 
 
 
-///test Florin
+///test Florin 16:07
 public class GUI extends JFrame implements ListSelectionListener, ActionListener{
 
 	private static Connection conn;
